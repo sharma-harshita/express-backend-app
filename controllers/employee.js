bec201839886c87b0b9677dbec02b1acdd376769
harshita.sharma <harshita.sharma@asksid.ai>
2022-02-15 21:00:02 +0530

    changes

--- controllers/employee.js
+++ controllers/employee.js
@@ -1,15 +1,18 @@
 const get = (req, res) => {
-    console.log("hello");
-    console.log("**************REQUEST query****************", req.query);
+    // console.log("**************REQUEST query****************", req.query);
     console.log("**************REQUEST params****************", req.params);
 
     const user = [
-        {id:1, name:"pooja"},
-        {id:2, name:"janaki"},
-        {id:3, name:"mani"},
+        {id:1, name:"pooja", category:"MERN"},
+        {id:2, name:"janaki", category:"MEAN"},
+        {id:3, name:"mani", category:"MERN"},
     ]
+
     if(req.query.id){
         const newUser = user.filter(value => value.id === Number(req.query.id))
+        res.send(newUser);
+    }else if(req.params.category){
+        const newUser = user.filter(value => value.category === req.params.category)
         res.send(newUser);
     }else{
         res.send(user);
--- routes/employee.js
+++ routes/employee.js
@@ -2,13 +2,17 @@ const express = require("express");
 const employeeController = require("../controllers/employee")
 const employeeRouter = express.Router();
 
-employeeRouter.route("/details/:id")
+
+employeeRouter.route("/details")
     .get(employeeController.employeeGet)
 
 
 
-// when you have url params in your api
-employeeRouter.route("/details/:id")
+// You dont have to change the route when you are passing the data from front end in the form of query params, because we can access those values using keys , which is part of query param only
+
+// But in case of URL params you will have to modify the route , because we need a key to access the value from url.
+
+employeeRouter.route("/details/:category")
 .get(employeeController.employeeGet)
 
 
